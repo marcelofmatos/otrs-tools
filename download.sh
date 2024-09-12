6a64ca52075580ead0a8825698a43af2bb3efd61
Marcelo Matos <marcelofmatos@gmail.com>
2024-09-12 11:39:02 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,5 @@
+# curl https://raw.githubusercontent.com/marcelofmatos/otrs-tools/main/download.sh | bash; perl scripts/ldap_connection_test.pl
+
 curl https://raw.githubusercontent.com/marcelofmatos/otrs-tools/main/scripts/ldap_auth_check.pl      > /opt/otrs/scripts/ldap_auth_check.pl
 curl https://raw.githubusercontent.com/marcelofmatos/otrs-tools/main/scripts/ldap_connection_test.pl > /opt/otrs/scripts/ldap_connection_test.pl
 curl https://raw.githubusercontent.com/marcelofmatos/otrs-tools/main/scripts/ldap_search_test.pl     > /opt/otrs/scripts/ldap_search_test.pl
